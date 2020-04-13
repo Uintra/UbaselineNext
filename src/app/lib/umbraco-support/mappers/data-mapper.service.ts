7ef53f22d31143263c93e416dfb371a237a854c4
Taras Gordienko <goldsteals@gmail.com>
2020-04-13 17:44:30 +0300

    refactor mapper

--- src/app/lib/umbraco-support/mappers/data-mapper.service.ts
+++ src/app/lib/umbraco-support/mappers/data-mapper.service.ts
@@ -1,39 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DataMapperService {
-
-  constructor() { }
+  constructor() {}
 
   private mapper(obj) {
     const isArray = Array.isArray(obj);
     const iterable = this.getIterableObject(obj);
-  ​
-    iterable &&
-      iterable.forEach((value, i, arr) => {
-        let current = isArray ? value : obj[value];
-  ​
-        if (this.isObject(current)) {
-          if (current.alias) {
-            if (!isArray) {
-              obj[value] = current.value;
-            } else {
-              arr[i] = current.value;
-            }
-            current = current.value;
+
+    iterable.forEach((value, i, arr) => {
+      let current = isArray ? value : obj[value];
+
+      if (this.isObject(current)) {
+        if (current.alias) {
+          if (!isArray) {
+            obj[value] = current.value;
+          } else {
+            arr[i] = current.value;
           }
-  ​
-          this.mapper(isArray ? current : obj[value]);
+          current = current.value;
         }
-      });
+        this.mapper(isArray ? current : obj[value]);
+      }
+    });
   }
-  ​
   private isObject(obj): boolean {
     return typeof obj === "object" && obj !== null;
   }
-  ​
   private getIterableObject(value): any[] | null {
     if (Array.isArray(value)) {
       return value;
@@ -41,11 +36,10 @@ export class DataMapperService {
     if (this.isObject(value)) {
       return Object.keys(value);
     }
-    return null;
-  };
-  ​
+    return [];
+  }
   public mapData(obj) {
     this.mapper(obj);
     return obj;
-  };
+  }
 }
